f231084229c372544d5b4def608e114c360f6b87
rocky-black-Wang <59869736+rocky-black-Wang@users.noreply.github.com>
2025-08-26 19:08:01 +0800

    Update susttrack_st.py

--- lib/test/parameter/susttrack_st.py
+++ lib/test/parameter/susttrack_st.py
@@ -1,7 +1,7 @@
 from lib.test.utils import TrackerParams
 import os
 from lib.test.evaluation.environment import env_settings
-from lib.config.stark_st2.config import cfg, update_config_from_file
+from lib.config.susttrack_st2.config import cfg, update_config_from_file
 
 
 def parameters(yaml_name: str):
